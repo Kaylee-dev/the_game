24b82a3f5ab2ef0bcd14cadfa730f853accecc17
Kaylee <99057337@mydavinci.nl>
2020-09-23 15:16:23 +0200

    Even proberen

--- script.js
+++ script.js
@@ -13,8 +13,17 @@ var begin = prompt("Je word wakker op een vreemde plek. Je ziet een pad naar een
 			
 			if (outOfHouse == "rechts") {
 				document.write ("Rechts")
-				var right = prompt("Je gaat naar rechts en ziet daar een wizard staan. Je loopt naar hem toe en hij zegt 'Kies een nummer tussen 1-10. Als het nummer hoger is dan mijn nummer kan je verder' Kies een nummer tussen 1 en 10:");
-			
+				var wizard = prompt("Je gaat naar rechts en ziet daar een wizard staan. Je loopt naar hem toe en hij zegt 'Kies een nummer tussen 1-10. Als het nummer hoger is dan mijn nummer kan je verder' Kies een nummer tussen 1 en 10:");
+				wizard = parseInt(wizard);
+				if (wizard > 5) {
+					if (wizard > 10) {
+						alert("Game Over")
+					}
+				} else {
+					alert("'Het getal is lager dan mijn getal, helaas je mag er niet langs.'")
+					alert("Game Over")
+				}
+				
 			} else if (outOfHouse == "links") {
 				document.write("Links")
 				var left = prompt("Je gaat naar links en ziet daar een tas liggen. In de tas zit een groen drankje. Wil je het drankje op drinken? Ja of Nee?")
@@ -22,9 +31,12 @@ var begin = prompt("Je word wakker op een vreemde plek. Je ziet een pad naar een
 
 		} else if (house == "schild") {
 			document.write("Schild")
-			alert("Je probeert het schild van de muur af te trekken, helaas lukt dit niet en valt er een plank op je hoofd. Game over");
+			alert("Je probeert het schild van de muur af te trekken, helaas lukt dit niet en vallen er planken op je hoofd.");
+			alert("Game Over")
 		}
 	} else if (begin == "bos") {
 		document.write("Bos")
-		var forestEnd = alert("Je loopt het bos in en komt een monster tegen. Je hebt geen zwaard en je kan het monster niet verslaan. Game over")
+		alert("Je loopt het bos in en komt een monster tegen. Je hebt geen zwaard en je kan het monster niet verslaan.")
+		alert("Game Over")
+
 	}
