6cdafba5c209849d32173b748913017ae3ae5c93
Kaylee <99057337@mydavinci.nl>
2020-09-25 11:44:23 +0200

    begin gemaakt

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 alert("Welkom bij mijn text adventure 'The Woods'");
 document.write ("<b>Jouw keuze's: <br>");
+var strength = 0;
 
 var begin = prompt("Je word wakker op een vreemde plek. Je ziet een pad naar een huisje en een pad naar het bos. Wat kies je? Huisje of Bos");
 	if (begin == "huisje") {
@@ -8,23 +9,43 @@ var begin = prompt("Je word wakker op een vreemde plek. Je ziet een pad naar een
 		var house = prompt("Je loopt naar het huisje en doet de deur open. In het huisje zie je een zwaard en een schild aan de muur. Welke kies je om mee te nemen? Keuze uit: Zwaard of Schild.");
 		
 		if (house == "zwaard") {
+			strength += 1;
 			document.write ("Zwaard<br>")
-			var outOfHouse = prompt("Je neemt het zwaard mee. Je loopt het bos in en komt een monster tegen. Je pakt je zwaard en je verslaat het monster. Je loopt weer verder en je komt op een splitsing. Wil je naar rechts of links?")
-			
-			if (outOfHouse == "rechts") {
+			var fight = prompt("Je neemt het zwaard mee. Je loopt het bos in en komt een monster tegen. Wil je vechten met het monster? Ja of Nee?");
+
+			if (fight == "ja") {
+				strength += 1;
+				alert("Je verslaat het monster met je zwaard");
+			} else if (fight == "nee") {
+				alert("Je rent weg van het monster");
+			}
+
+			var path = prompt("Je loopt verder en komt bij een splitsing aan. Wil je naar Rechts of Links?");
+			if (path == "rechts") {
 				document.write ("Rechts")
-				var wizard = prompt("Je gaat naar rechts en ziet daar een wizard staan. Je loopt naar hem toe en hij zegt 'Kies een nummer tussen 1-10. Als het nummer hoger is dan mijn nummer kan je verder' Kies een nummer tussen 1 en 10:");
+				var wizard = prompt("Je gaat naar rechts en ziet daar een wizard staan. Je loopt naar hem toe en hij zegt 'Ik heb een getal in mijn hoofd raad hoger voor een verassing.' Kies een nummer tussen 0-10.");
 				wizard = parseInt(wizard);
 				if (wizard > 5) {
 					if (wizard > 10) {
-						alert("Game Over")
+						alert("'Dat is hoger dan 10, dat mag niet' De wizard word boos en vermoord je.");
+						alert("Game Over");
+
+					} else {
+						alert("'Goed geraden, hier heb je een verassing.' De wizard spreekt een spreuk uit. Je voelt je sterker. ");
+						strength += 1;
 					}
-				} else {
-					alert("'Het getal is lager dan mijn getal, helaas je mag er niet langs.'")
-					alert("Game Over")
+
+
+				} else if(wizard <= 5){
+					alert("'Helaas dit is te laag. Je krijgt niks.' Je loopt verder.");
+					var water = prompt("Je komt aan bij een rivier. Je ziet rechts een oude houten boot liggen wil je deze gebruiken om de rivier over te steken? Ja of Nee? ");
+
+					if (water == "Ja") {
+						
+					}
 				}
 				
-			} else if (outOfHouse == "links") {
+			} else if (path == "links") {
 				document.write("Links")
 				var left = prompt("Je gaat naar links en ziet daar een tas liggen. In de tas zit een groen drankje. Wil je het drankje op drinken? Ja of Nee?")
 			}
